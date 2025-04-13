103328e040145933c02582ecbfe6246f7d43b251
Gia Phat <giaphat@example.com>
2025-04-13 12:02:54 +0700

    tesst failllll

--- src/test/java/com/poly/Caculator.java
+++ src/test/java/com/poly/Caculator.java
@@ -4,12 +4,12 @@ import com.poly.Icaculator;
 
 public class Caculator implements Icaculator{
 	public Integer sum(Integer a, Integer b) {
-		return a+b;
+		return 0;
 
 	}
 	
 	public Integer sub(Integer a, Integer b) {
-		return a+b;
+		return 0;
 
 	}
 }
